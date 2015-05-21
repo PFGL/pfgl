4b02b8b613e3194bf06737bb1de9be41caf6de3d
jackmachin <jack.machin125@gmail.com>
2015-05-21 10:12:14 +0100

    Winnar

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
             <div id="inner-header" class="wrapper">
                 <div class="logo-container">
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-                    <p id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+                    <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="<?php bloginfo('name'); ?>" width="417" height="107"></a>
 
                     <?php // if you'd like to use the site description you can un-comment it below ?>
                     <?php // bloginfo('description'); ?>
