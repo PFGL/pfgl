99018644a42294000c45fbc782fb4ec95014d09d
jackmachin <jack.machin125@gmail.com>
2015-05-21 15:30:01 +0100

    Good hair

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
             <div id="inner-header" class="wrapper">
                 <div class="logo-container">
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-                    <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="<?php bloginfo('name'); ?>" width="417" height="107"></a>
+                    <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="<?php bloginfo('name'); ?>" width="417" height="107" class="logo"></a>
 
                     <?php // if you'd like to use the site description you can un-comment it below ?>
                     <?php // bloginfo('description'); ?>
