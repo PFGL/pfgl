57a0f7cff1f3f925e06a6bbc7d98304651547df1
jackmachin <jack.machin125@gmail.com>
2015-05-21 14:03:24 +0100

    Test

--- header.php
+++ header.php
@@ -36,6 +36,7 @@
 
 		<?php // drop Google Analytics Here ?>
 		<?php // end analytics ?>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 
 	</head>
 
