c1c9ef0fbefa31607fbf2ba0ba3146b464a695ea
jackmachin <jack.machin125@gmail.com>
2015-07-10 15:40:00 +0100

    MadebySpark

--- single.php
+++ single.php
@@ -1,56 +1,53 @@
 <?php get_header(); ?>
 
-			<div id="content" class="wrapper">
+    <div id="content" class="wrapper">
 
-				<div id="inner-content" class="container">
+        <div id="inner-content" class="container">
 
-					<div class="main" role="main">
+            <div class="main" role="main">
 
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-                            <header class="article-header">
+                    <header class="article-header">
 
-                              <h1 class="entry-title single-title" itemprop="headline">
-                                  <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                              </h1>
+                        <h1 class="entry-title single-title" itemprop="headline">
+                            <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                      </h1>
 
-                            </header> <?php // end article header ?>
+                    </header> <?php // end article header ?>
 
+                    <div class="entry-content" itemprop="articleBody">
+                      <?php
+                        // the content (pretty self explanatory huh)
+                        the_content();
+                      ?>
+                    </div> <?php // end article section ?>
 
-                <div class="entry-content" itemprop="articleBody">
-                  <?php
-                    // the content (pretty self explanatory huh)
-                    the_content();
-                  ?>
-                </div> <?php // end article section ?>
+                    <footer class="article-footer">
 
-                <footer class="article-footer">
+                      <?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
 
-                  <?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
+                      <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+                    </footer> <?php // end article footer ?>
 
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+                  </article> <?php // end article ?>
 
-                </footer> <?php // end article footer ?>
+                <?php endwhile; ?>
 
-              </article> <?php // end article ?>
+                <?php else : ?>
 
+                    <?php get_template_part ('partials/no-post-found');?>
 
-						<?php endwhile; ?>
+                <?php endif; ?>
 
-						<?php else : ?>
+            </div>
 
-                            <?php get_template_part ('partials/no-post-found');?>
+            <?php get_sidebar(); ?>
 
-						<?php endif; ?>
+        </div>
 
-					</div>
-
-					<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+    </div>
 
 <?php get_footer(); ?>
