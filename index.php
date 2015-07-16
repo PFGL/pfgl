311dda089b49dd9ff7065db07423236a49f31bb8
jackmachin <jack.machin125@gmail.com>
2015-07-16 11:22:02 +0100

    It was just banter

--- index.php
+++ index.php
@@ -8,25 +8,21 @@
 
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-              <?php
-                /*
-                 * This is the default post format.
-                 *
-                 * So basically this is a regular post. if you don't want to use post formats,
-                 * you can just copy ths stuff in here and replace the post format thing in
-                 * single.php.
-                 *
-                 * The other formats are SUPER basic so you can style them as you like.
-                 *
-                 * Again, If you want to remove post formats, just delete the post-formats
-                 * folder and replace the function below with the contents of the "format.php" file.
-                */
-              ?>
+                <header class="article-header">
 
-              <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                    <div class="date">
+						<span class="day"><?php the_time('d') ?> </span>
+						<span class="month"><?php the_time('M'); ?></span>
+					</div><!--date-->
 
-                <?php get_template_part ('partials/article-header');?>
+                  <h1 class="entry-title single-title" itemprop="headline">
+                      <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                  </h1>
+
+                </header> <?php // end article header ?>
+
 
                 <div class="entry-content" itemprop="articleBody">
                   <?php
