2e579391e207b481c74b1669ae8f1e0b6c0a551d
jackmachin <jack.machin125@gmail.com>
2015-07-16 11:00:36 +0100

    Year of luigi

--- index.php
+++ index.php
@@ -24,14 +24,14 @@
                 */
               ?>
 
-              <article id="post-<?php the_ID(); ?>" <?php post_class('fuck-partials'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+              <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
                 <?php get_template_part ('partials/article-header');?>
 
                 <div class="entry-content" itemprop="articleBody">
                   <?php
                     // the content (pretty self explanatory huh)
-                    the_content();
+                    the_excerpt();
                   ?>
                 </div> <?php // end article section ?>
 
